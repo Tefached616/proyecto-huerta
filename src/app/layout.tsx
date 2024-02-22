d8f58865ea914144a1670eb9c88a91cf9e0aa2f1
xdiego96 <zdiego96@gmail.com>
2024-02-22 10:38:23 -0500

    hecho

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs'
+//import { ClerkProvider } from '@clerk/nextjs'
 
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
@@ -15,10 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+   // <ClerkProvider>
     <html lang='es'>
       <body className={poppins.className}>{children}</body>
     </html>
-    </ClerkProvider>
+    //</ClerkProvider>
   );
 }
